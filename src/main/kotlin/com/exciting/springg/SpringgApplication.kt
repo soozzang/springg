9fe55458eb56006649a4007ebf0c1433d45a51c0
한수현 Soohyun Han <121356687+soozzang@users.noreply.github.com>
2024-10-29 22:52:31 +0900

    init

---
+++ src/main/kotlin/com/exciting/springg/SpringgApplication.kt
@@ -0,0 +1,11 @@
+package com.exciting.springg
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringgApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringgApplication>(*args)
+}
---
+++ src/test/kotlin/com/exciting/springg/SpringgApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.exciting.springg
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringgApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
