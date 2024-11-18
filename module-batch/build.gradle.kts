8f0a20a25d4bd678f1719462c757062617c3a7bd
soozzang <devsoo511@gmail.com>
2024-11-18 10:07:47 +0900

    .

---
+++ module-batch/build.gradle.kts
@@ -0,0 +1,17 @@
+//group = "com.exciting"
+//version = "1.0-SNAPSHOT"
+
+//repositories {
+//    mavenCentral()
+//}
+
+dependencies {
+//    testImplementation(kotlin("test"))
+}
+
+//tasks.test {
+//    useJUnitPlatform()
+//}
+//kotlin {
+//    jvmToolchain(21)
+//}
